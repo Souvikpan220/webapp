9495c2ecf89bab087fe65efcfb40db285a8e89f6
Souvikpan220 <117498210+Souvikpan220@users.noreply.github.com>
2026-05-13 20:59:01 +0530

    Update AuthContext.jsx

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -1,1 +1,27 @@
+import { createContext, useContext, useState } from 'react'
 
+const AuthContext = createContext()
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem('session')) || null
+  )
+
+  const login = (data) => {
+    localStorage.setItem('session', JSON.stringify(data))
+    setUser(data)
+  }
+
+  const logout = () => {
+    localStorage.removeItem('session')
+    setUser(null)
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export const useAuth = () => useContext(AuthContext)
