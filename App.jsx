56de45e604cee2ea2d117e7e65471fc1476cb379
Souvikpan220 <117498210+Souvikpan220@users.noreply.github.com>
2026-05-13 20:58:12 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,1 +1,26 @@
+import { Routes, Route, Navigate } from 'react-router-dom'
+import Login from './pages/Login'
+import Setup from './pages/Setup'
+import Home from './pages/Home'
+import SplashScreen from './components/ui/SplashScreen'
+import { useEffect, useState } from 'react'
 
+export default function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 2000)
+    return () => clearTimeout(timer)
+  }, [])
+
+  if (loading) return <SplashScreen />
+
+  return (
+    <Routes>
+      <Route path='/' element={<Login />} />
+      <Route path='/setup' element={<Setup />} />
+      <Route path='/home' element={<Home />} />
+      <Route path='*' element={<Navigate to='/' />} />
+    </Routes>
+  )
+}
